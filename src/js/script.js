289c95252cef9ae08e0a050b0f0524a385258725
ivanbohdanov <ioan.bohdanov@gmail.com>
2015-05-25 19:31:53 +0300

    refactored work section

--- src/js/script.js
+++ src/js/script.js
@@ -7,10 +7,12 @@
 		//add class to section tag for appearance of needs block
 		//
 		var that = this;
+		$('nav a').removeClass('is-selected');
 		if (!$('body').hasClass('active')) {
 			$('section.first').fadeOut(300);
 			$('header').fadeOut(0);
 			setTimeout(function () {
+				$(that).addClass('is-selected');
 				var linkName = $(that).text();
 				$('section').removeClass('is-selected');
 				$('.' + linkName).addClass('is-selected');
@@ -18,6 +20,7 @@
 			}, 500);
 			$('body').addClass('active');
 		} else {
+			$(that).addClass('is-selected');
 			setTimeout(function() {
 				var linkName = $(that).text();
 				$('section').removeClass('is-selected').fadeOut(0);
