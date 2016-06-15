5c1983252520903b7f19530c8972920c40cdf64b
ivanbohdanov <ioan.bohdanov@gmail.com>
2016-06-15 18:08:22 +0300

    check video.js api

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,22 @@ $(document).ready(function () {
   // $(document).on('load', function () {
   //   secondVideo.play()
   // });
-  
+  function triggerClick(element) {
+    if(document.createEvent) {
+      console.log(element)
+        var evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        element.dispatchEvent(evt);
+    }
+    else {
+        element.click();
+    }
+  } 
   videojs('third').src("/video.mov").ready(function () {
       this.play();
   })
   setTimeout(function () {
-    $('#test').trigger( "click" )
+    triggerClick($('#test').get()[0])
   }, 2000)
+
 })
