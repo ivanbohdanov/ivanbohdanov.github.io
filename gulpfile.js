cee3a20b08d750e28292dbe0c1fa9e783b4abee5
ivanbohdanov <ioan.bohdanov@gmail.com>
2016-06-15 17:17:53 +0300

    check video.js api

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  pleeease = require('gulp-pleeease'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  plumber = require('gulp-plumber'),
-  imagemin = require('gulp-imagemin'),
+  // sass = require('gulp-sass'),
+  // pleeease = require('gulp-pleeease'),
+  // concat = require('gulp-concat'),
+  // uglify = require('gulp-uglify'),
+  // plumber = require('gulp-plumber'),
+  // imagemin = require('gulp-imagemin'),
   browserSync = require('browser-sync'),
   reload      = browserSync.reload;
 
 // Sass
 
-gulp.task('sass', function () {
-  gulp.src('src/scss/*.scss')
-    .pipe(sass({errLogToConsole: true})) // Keep running gulp even though occurred compile error
-    .pipe(pleeease({
-        autoprefixer: {
-            browsers: ['> 0.01%']
-        }
-    }))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(reload({stream:true}));
-});
+// gulp.task('sass', function () {
+//   gulp.src('src/scss/*.scss')
+//     .pipe(sass({errLogToConsole: true})) // Keep running gulp even though occurred compile error
+//     .pipe(pleeease({
+//         autoprefixer: {
+//             browsers: ['> 0.01%']
+//         }
+//     }))
+//     .pipe(gulp.dest('dist/css'))
+//     .pipe(reload({stream:true}));
+// });
 
 // Js-concat-uglify
 
-gulp.task('js', function() {
-  gulp.src(['src/js/*.js'])
-    .pipe(plumber()) //Keep running gulp even though occurred compile error for js
-    .pipe(concat('scripts.js'))
-    .pipe(uglify({preserveComments: 'some'})) // Keep some comments
-    .pipe(gulp.dest('dist/js'))
-    .pipe(reload({stream:true}));
-});
+// gulp.task('js', function() {
+//   gulp.src(['src/js/*.js'])
+//     .pipe(plumber()) //Keep running gulp even though occurred compile error for js
+//     .pipe(concat('scripts.js'))
+//     .pipe(uglify({preserveComments: 'some'})) // Keep some comments
+//     .pipe(gulp.dest('dist/js'))
+//     .pipe(reload({stream:true}));
+// });
 
 // Imagemin
 
-gulp.task('imagemin', function() {
-  gulp.src(['src/img/*.{png,jpg,gif,svg}'])
-    .pipe(imagemin({optimizationLevel: 7}))
-    .pipe(gulp.dest('dist/img'));
-});
+// gulp.task('imagemin', function() {
+//   gulp.src(['src/img/*.{png,jpg,gif,svg}'])
+//     .pipe(imagemin({optimizationLevel: 7}))
+//     .pipe(gulp.dest('dist/img'));
+// });
 
 // Static server
 
@@ -60,8 +60,8 @@ gulp.task('bs-reload', function () {
 // Task for `gulp` command
 
 gulp.task('default',['browser-sync'], function() {
-  gulp.watch('src/scss/**/*.scss',['sass']);
-  gulp.watch('src/js/*.js',['js']);
-  gulp.watch('src/img/**/*.{png,jpg,gif,svg}',['imagemin']);
+  // gulp.watch('src/scss/**/*.scss',['sass']);
+  // gulp.watch('src/js/*.js',['js']);
+  // gulp.watch('src/img/**/*.{png,jpg,gif,svg}',['imagemin']);
   gulp.watch("*.html", ['bs-reload']);
 });
