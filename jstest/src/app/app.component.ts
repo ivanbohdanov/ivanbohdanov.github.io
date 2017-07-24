375e77a695beae1f6416d89f4871a62718879b59
ivanbohdanov <ioan.bohdanov@gmail.com>
2017-07-25 02:56:09 +0300

    added download file method and some murkup for jstest

---
+++ jstest/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Questions } from '../data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  constructor() {}
+
+  public window = window as any;
+  public questions = Questions;
+  public userName: string;
+
+  ngAfterViewInit() {
+    this.window.initHighlightingOnLoad();
+  }
+
+  downloadFile(data: Response) {
+    let parsedResponse = 'test';
+    let blob = new Blob([parsedResponse], { type: 'text/csv' });
+    let url = window.URL.createObjectURL(blob);
+
+    if(navigator.msSaveOrOpenBlob) {
+        navigator.msSaveBlob(blob, `Answers_from_${this.userName.replace('/ /g', '_')}.json`);
+    } else {
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = `Answers_from_${this.userName.replace('/ /g', '_')}.json`;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+    window.URL.revokeObjectURL(url);
+  }
+
+}
---
+++ jstest/src/data.ts
@@ -0,0 +1,35 @@
+export const Questions = [
+    {
+        "questionName": "question1",
+        "questionDescription": "description",
+        "questionType": 'radio',
+        "options": [
+            "value1",
+            "value2",
+            "value3",
+            "value4"
+        ]
+    },
+    {
+        "questionName": "question2",
+        "questionDescription": "description",
+        "questionType": 'text',
+        "options": [
+            "value1",
+            "value2",
+            "value3",
+            "value4"
+        ]
+    },
+    {
+        "questionName": "question3",
+        "questionDescription": "description",
+        "questionType": 'checkbox',
+        "options": [
+            "value1",
+            "value2",
+            "value3",
+            "value4"
+        ]
+    },
+]
