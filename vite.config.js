074f3025c65090433c86d4ed1cfa2f52de07697c
ivan.bohdanov <ivan.bohdanov@rubyplay.com>
2024-05-16 00:14:20 +0300

    added vite and new site

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import Icons from 'unplugin-icons/vite'
+
+export default defineConfig({
+    publicDir: "public",
+    root: "./",
+    build: {
+        outDir: "dist",
+    },
+    plugins: [
+        eslint({
+            cache: false,
+            fix: true,
+        }),
+        Icons({compiler: "raw"}),
+    ],
+});
